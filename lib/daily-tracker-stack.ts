c1ba94518774193bc80afcd4c42819ffffd61822
8888 <leecostello1@gmail.com>
2022-05-15 13:10:52 -0400

    add a pipeline construct with a CodePipeline

--- lib/daily-tracker-stack.ts
+++ lib/daily-tracker-stack.ts
@@ -1,16 +1,11 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Pipeline } from './pipeline';
 
 export class DailyTrackerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'DailyTrackerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new Pipeline(this, 'DailyTrackerPipeline');
   }
 }
---
+++ lib/pipeline.ts
@@ -0,0 +1,20 @@
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { Construct } from 'constructs';
+
+export class Pipeline extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const pipeline = new CodePipeline(this, 'DailyTrackerCodePipeline', {
+      pipelineName: 'DailyTrackerPipeline',
+      crossAccountKeys: true,
+      dockerEnabledForSynth: true,
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.connection('8888/daily-tracker', 'main', {
+          connectionArn: 'arn:aws:codestar-connections:us-east-1:293675922272:connection/63408913-2605-497c-94f6-0945b4152d8d',
+        }),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+      }),
+    });
+  }
+}
