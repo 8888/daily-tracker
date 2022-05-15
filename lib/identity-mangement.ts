25ae68aa38bcbba09cd59fa35e0d4bcfbe220b2a
8888 <leecostello1@gmail.com>
2022-05-15 14:43:17 -0400

    add identity mangement construct for cognito

---
+++ lib/identity-mangement.ts
@@ -0,0 +1,44 @@
+import { Construct } from 'constructs';
+import { OAuthScope, UserPool } from 'aws-cdk-lib/aws-cognito';
+
+export class IdentityManagement extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const userPool = new UserPool(this, 'UserPool', {
+      userPoolName: 'DailyTracker-UserPool',
+      selfSignUpEnabled: false,
+      userInvitation: {
+        emailSubject: 'Invite to join Daily Tracker!',
+        emailBody: 'Hello {username}, you have been invited to join Daily Tracker! Your temporary password is {####}',
+      },
+      signInAliases: {
+        email: true,
+        username: false,
+      },
+      autoVerify: { email: true },
+      standardAttributes: {
+        givenName: { required: true },
+        familyName: { required: true },
+        email: { required: true },
+      },
+    });
+
+    userPool.addClient('customer-app-client', {
+      authFlows: {
+        userPassword: true,
+        userSrp: true,
+      },
+      oAuth: {
+        flows: { authorizationCodeGrant: true },
+        scopes: [ OAuthScope.OPENID ],
+        callbackUrls: [ 'www.example.com' ],
+      },
+      preventUserExistenceErrors: true,
+    });
+
+    userPool.addDomain('CognitoDomain', {
+      cognitoDomain: { domainPrefix: 'daily-tracker' },
+    });
+  }
+}
