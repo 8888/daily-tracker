8824b97f93eeb8cedb2a15ebc67cd34165668c58
8888 <leecostello1@gmail.com>
2022-05-16 08:50:03 -0400

    debug the working directory during synth

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -14,12 +14,14 @@ export class Pipeline extends Construct {
     const buildAngularAction = new CodeBuildStep('Build', {
       input: repo,
       installCommands: [
+        'pwd',
         'cd lib/client',
         'npm ci',
         'wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb',
         'apt install ./google-chrome-stable_current_amd64.deb',
       ],
       commands: [
+        'pwd',
         'npm run test:ci',
         'npm run build',
       ],
