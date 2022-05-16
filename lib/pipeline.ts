800036a32f18005dd79679d3a377ab304ca11b97
8888 <leecostello1@gmail.com>
2022-05-15 22:01:01 -0400

    update pipeline to build angular app

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -1,4 +1,5 @@
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { LinuxBuildImage } from 'aws-cdk-lib/aws-codebuild';
+import { CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 import { AppStage } from './app-stage';
 
@@ -6,16 +7,42 @@ export class Pipeline extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
+    const repo = CodePipelineSource.connection('8888/daily-tracker', 'main', {
+      connectionArn: 'arn:aws:codestar-connections:us-east-1:293675922272:connection/63408913-2605-497c-94f6-0945b4152d8d',
+    });
+
+    const buildAngularAction = new CodeBuildStep('Build', {
+      input: repo,
+      installCommands: [
+        'npm ci',
+        'wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb',
+        'apt install ./google-chrome-stable_current_amd64.deb',
+      ],
+      commands: [
+        'npm run test:ci',
+        'npm run build',
+      ],
+      buildEnvironment: {
+        buildImage: LinuxBuildImage.STANDARD_5_0,
+      },
+      primaryOutputDirectory: './',
+    });
+
+    const synthAction = new ShellStep('Synth', {
+      input: buildAngularAction,
+      commands: [
+        'npm ci',
+        'npm run build',
+        'npx cdk synth',
+      ],
+      primaryOutputDirectory: 'cdk.out',
+    });
+
     const pipeline = new CodePipeline(this, 'DailyTrackerCodePipeline', {
       pipelineName: 'DailyTrackerPipeline',
       crossAccountKeys: true,
-      dockerEnabledForSynth: true,
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.connection('8888/daily-tracker', 'main', {
-          connectionArn: 'arn:aws:codestar-connections:us-east-1:293675922272:connection/63408913-2605-497c-94f6-0945b4152d8d',
-        }),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
-      }),
+      dockerEnabledForSelfMutation: true,
+      synth: synthAction,
     });
 
     pipeline.addStage(new AppStage(this, 'DailyTrackerAppStage', {
