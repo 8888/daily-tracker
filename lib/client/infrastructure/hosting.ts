0a543b88eaad6b6ad3c06d0f87d34ba97cad9842
8888 <leecostello1@gmail.com>
2022-05-15 21:46:14 -0400

    add hosting construct for s3 hosting and cloudfront distro

---
+++ lib/client/infrastructure/hosting.ts
@@ -0,0 +1,51 @@
+import { CloudFrontAllowedMethods, CloudFrontWebDistribution, OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
+import { CanonicalUserPrincipal, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { Construct } from 'constructs';
+
+export class Hosting extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const hostingBucket = new Bucket(this, 'DailyTrackerHostingBucket', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: false,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+    });
+
+    const cloudfrontOAI = new OriginAccessIdentity(this, 'cloudfront-OAI', {
+      comment: `OAI for ${id}`,
+    });
+
+    // grant access to CloudFront
+    hostingBucket.addToResourcePolicy(new PolicyStatement({
+      actions: [ 's3:GetObject' ],
+      resources: [ hostingBucket.arnForObjects('*') ],
+      principals: [ new CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId) ],
+    }));
+
+    const distribution = new CloudFrontWebDistribution(this, 'SiteDistribution', {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: hostingBucket,
+            originAccessIdentity: cloudfrontOAI,
+          },
+          behaviors: [{
+            isDefaultBehavior: true,
+            compress: true,
+            allowedMethods: CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+          }],
+        },
+      ],
+    });
+
+    new BucketDeployment(this, 'DeployWithInvalidation', {
+      sources: [ Source.asset('./lib/client/dist/daily-tracker') ],
+      destinationBucket: hostingBucket,
+      distribution,
+      distributionPaths: ['/*'], // this invalidates the cloudfront distribution
+    });
+  }
+}
--- lib/daily-tracker-stack.ts
+++ lib/daily-tracker-stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Hosting } from './client/infrastructure/hosting';
 import { IdentityManagement } from './identity-management';
 
 export class DailyTrackerStack extends Stack {
@@ -7,5 +8,6 @@ export class DailyTrackerStack extends Stack {
     super(scope, id, props);
 
     new IdentityManagement(this, 'DailyTrackerIdM');
+    new Hosting(this, 'DailyTrackerClientHosting');
   }
 }
