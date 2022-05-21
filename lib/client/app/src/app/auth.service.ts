79ad79eb6dc7535fe1de8aa520af9b5f1038cf9e
8888 <leecostello1@gmail.com>
2022-05-20 22:07:13 -0400

    add method to logout

--- lib/client/app/src/app/auth.service.ts
+++ lib/client/app/src/app/auth.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable } from '@angular/core';
+
+import { environment } from 'src/environments/environment';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
   public idToken = '';
   public accessToken = '';
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+  ) {}
+
+  public logout(): void {
+    localStorage.clear();
+    this.document.location.href = environment.loginUrl;
+  }
 
   private storeTokens(id: string, access: string): void {
     this.idToken = id;
