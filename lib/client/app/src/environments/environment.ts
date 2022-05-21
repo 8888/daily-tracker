7ae78a60be9b47883f5968c9255c2d1bb569e19f
8888 <leecostello1@gmail.com>
2022-05-20 22:00:19 -0400

    configure angular dev environment variables for cognito

--- lib/client/app/src/environments/environment.ts
+++ lib/client/app/src/environments/environment.ts
@@ -1,9 +1,18 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+const angularAppUrl = 'http://0.0.0.0:8888/';
+const cognitoUrl = 'http://0.0.0.0:9229/';
+
+const cognitoAuthUrl = `${cognitoUrl}oauth2/token`;
+
+const userPoolClientId = 'local_4Js6VY0h';
+const userPoolClientRedirectUri = angularAppUrl;
+const loginUrl = `${cognitoUrl}login?client_id=${userPoolClientId}&response_type=code&scope=openid&redirect_uri=${encodeURIComponent(userPoolClientRedirectUri)}`;
 
 export const environment = {
-  production: false
+  production: false,
+  cognitoAuthUrl,
+  userPoolClientId,
+  userPoolClientRedirectUri,
+  loginUrl,
 };
 
 /*
@@ -13,4 +22,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
