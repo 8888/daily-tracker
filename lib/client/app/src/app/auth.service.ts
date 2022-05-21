faee76bca9494aa8f4c7971dfb4767d2b1ce6c12
8888 <leecostello1@gmail.com>
2022-05-20 22:15:14 -0400

    functionality to retrieve tokens from a code

--- lib/client/app/src/app/auth.service.ts
+++ lib/client/app/src/app/auth.service.ts
@@ -1,4 +1,5 @@
 import { DOCUMENT } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 
 import { environment } from 'src/environments/environment';
@@ -10,11 +11,43 @@ export class AuthService {
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
+    private http: HttpClient,
   ) {}
 
   public logout(): void {
     localStorage.clear();
     this.document.location.href = environment.loginUrl;
+  }
+
+  private extractCode(): string {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+    return code || '';
+  }
+
+  private exchangeCodeForTokens(code: string, resolve: () => void, reject: () => void) {
+    const body = new URLSearchParams();
+    body.set('grant_type', 'authorization_code');
+    body.set('code', code);
+    body.set('client_id', environment.userPoolClientId);
+    body.set('redirect_uri', environment.userPoolClientRedirectUri);
+
+    const headers = { 'content-type': 'application/x-www-form-urlencoded' };
+
+    this.http.post<any>(
+      environment.cognitoAuthUrl,
+      body.toString(),
+      { headers },
+    ).subscribe({
+      next: response => {
+        this.storeTokens(response.id_token, response.access_token);
+        resolve();
+      },
+      error: () => {
+        this.logout();
+        reject();
+      },
+    });
   }
 
   private storeTokens(id: string, access: string): void {
