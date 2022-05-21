a180196d88221bc1715a9e5c79ffc9984a2a2d41
8888 <leecostello1@gmail.com>
2022-05-20 22:52:04 -0400

    configure prod environment

--- lib/client/app/src/environments/environment.prod.ts
+++ lib/client/app/src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
+const angularAppUrl = 'https://dailytracker.apphosting.link/';
+const cognitoUrl = 'https://auth.dailytracker.apphosting.link/';
+
+const cognitoAuthUrl = `${cognitoUrl}oauth2/token`;
+
+const userPoolClientId = '5o7drsppg73j3vu377vqnb6mat';
+const userPoolClientRedirectUri = angularAppUrl;
+const loginUrl = `${cognitoUrl}login?client_id=${userPoolClientId}&response_type=code&scope=openid&redirect_uri=${encodeURIComponent(userPoolClientRedirectUri)}`;
+
 export const environment = {
-  production: true
+  production: true,
+  cognitoAuthUrl,
+  userPoolClientId,
+  userPoolClientRedirectUri,
+  loginUrl,
 };
