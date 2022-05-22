b6bd059e17c073bff05e975509df4b6fa4a37b2a
8888 <leecostello1@gmail.com>
2022-05-22 11:17:16 -0400

    create serverless aurora cluster

---
+++ lib/database/aurora.ts
@@ -0,0 +1,30 @@
+import { SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { DatabaseClusterEngine, ParameterGroup, ServerlessCluster } from 'aws-cdk-lib/aws-rds';
+import { Construct } from 'constructs';
+
+export class AuroraDatabase extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const vpc = new Vpc(this, 'VPC', {
+      subnetConfiguration: [
+        {
+          name: 'Database',
+          subnetType: SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    });
+
+    const dbName = 'DailyTrackerDatabase';
+    const cluster = new ServerlessCluster(this, 'ServerlessAuroraCluster', {
+      defaultDatabaseName: dbName,
+      engine: DatabaseClusterEngine.AURORA_POSTGRESQL,
+      vpc,
+      enableDataApi: true,
+      vpcSubnets: {
+        subnetType: SubnetType.PRIVATE_ISOLATED,
+      },
+      parameterGroup: ParameterGroup.fromParameterGroupName(this, 'ParameterGroup', 'default.aurora-postgresql13')
+    });
+  }
+}
