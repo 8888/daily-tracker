1ed21921dc3253334954ae04bb847a9c8a9f5d2d
8888 <leecostello1@gmail.com>
2022-05-30 19:29:47 -0400

    update types to have ts compile for lambda

--- lib/database/aurora.migration.ts
+++ lib/database/aurora.migration.ts
@@ -1,10 +1,12 @@
-import client from 'data-api-client';
+import { Context } from 'aws-lambda';
+
+const client = require('data-api-client');
 
 const database = 'dailytracker';
 const resourceArn = 'arn:aws:rds:us-east-1:001812633811:cluster:dailytrackerappstage-dai-dailytrackerdbserverless-1ggazp1xm8n8d';
 const secretArn = 'arn:aws:secretsmanager:us-east-1:001812633811:secret:DailyTrackerDBServerlessAur-mdL2kArspLes-BnC2QY';
 
-exports.handler = async function(event, context) {
+exports.handler = async function(event: any, context: Context) {
   const db = client({ database, resourceArn, secretArn });
 
   const { records } = await db.query('select column_name from information_schema.columns;');
