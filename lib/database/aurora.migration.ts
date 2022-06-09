f179ee6b209eb076a4b089ab62667ed7c1fd2784
8888 <leecostello1@gmail.com>
2022-06-08 21:40:05 -0400

    update lambda to read contents of files

--- lib/database/aurora.migration.ts
+++ lib/database/aurora.migration.ts
@@ -11,32 +11,48 @@ const secretArn = 'arn:aws:secretsmanager:us-east-1:001812633811:secret:DailyTra
 
 const bucketName = process.env.BUCKET;
 
+const headers = { 'Access-Control-Allow-Origin': '*' };
+
 exports.handler = async function(event: any, context: Context) {
+  const schemaVersion = 0; // fetch from DB
+
   if (bucketName) {
     const migrationFiles = await s3.listObjectsV2({ Bucket: bucketName }).promise();
     const fileKeys = migrationFiles.Contents?.map(f => f.Key);
 
-    if (fileKeys) {
-      const file = await s3.getObject({
-        Bucket: bucketName,
-        Key: fileKeys[0]!,
+    if (fileKeys && fileKeys.length > schemaVersion) {
+      // run schema migrations
+      const keys = fileKeys.slice(schemaVersion);
+      const files = [];
+
+      keys.forEach(async key => {
+        const file = await s3.getObject({ Bucket: bucketName, Key: key! }).promise();
+        console.log('*** file contents ***')
+        console.log(file.Body?.toString());
+        files.push(file.Body?.toString());
       });
-      console.log(file);
+
+      const db = client({ database, resourceArn, secretArn });
+      const { records } = await db.query('select schema_name from information_schema.schemata;');
+      const body = { records, fileKeys };
+
+      return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify(body),
+      };
+    } else {
+      // no migrations to run
+      return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify({ message: 'No migrations to run!' }),
+      };
     }
-
-    const db = client({ database, resourceArn, secretArn });
-    const { records } = await db.query('select schema_name from information_schema.schemata;');
-    const body = { records, fileKeys };
-
-    return {
-      statusCode: 200,
-      headers: {'Access-Control-Allow-Origin': '*'},
-      body: JSON.stringify(body),
-    };
   } else {
     return {
       statusCode: 500,
-      headers: {'Access-Control-Allow-Origin': '*'},
+      headers,
       body: JSON.stringify({ message: 'No migrations bucket found!' }),
     };
   }
